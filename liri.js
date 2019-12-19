fffa12c148b380684496709431110c7828099bad
coltonfalkner <coltonfalkner@gmail.com>
2019-12-18 22:17:24 -0600

    'concert-this' code

--- liri.js
+++ liri.js
@@ -1,3 +1,47 @@
-var keys = require(".keys.js");
+require("dotenv").config();
+
+var keys = require("./keys");
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require("node-spotify-api");
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
+
+var defaultMovie= "Mr. Nobody";
+
+var action = process.argv[2];
+var value = process.argv[3];
+
+switch (action){
+    case "concert-this":
+        getBands(value)
+        break;
+    case "spotify-this-song":
+        getSongs(value)
+        break;
+    case "movie-this":
+        if (value == "0"){
+            value = defaultMovie;
+        }
+        getMovies(value)
+        break;
+    case "do-what-it-says":
+      doWhatItSays()
+      break;
+      default:
+      break;
+}
+
+function getBands(artist){
+  axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+    .then(function(response){
+      console.log("Name of Venue: " , response.data[0].venue.name);
+      console.log("Venue location: ", response.data[0].venue.city);
+      var eventDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
+      console.log("Date of Event: ", eventDate);
+    })
+    .catch(function (error){
+      console.log(error);
+    });
+}
